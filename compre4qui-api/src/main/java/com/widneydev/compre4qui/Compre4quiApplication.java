4bacf6ced9829a802fc1d014a3b1a442b0b8c9e3
Widney Lima <widneylima21@gmail.com>
2024-06-29 15:21:58 -0300

    api for the project

---
+++ compre4qui-api/src/main/java/com/widneydev/compre4qui/Compre4quiApplication.java
@@ -0,0 +1,13 @@
+package com.widneydev.compre4qui;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Compre4quiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Compre4quiApplication.class, args);
+	}
+
+}
---
+++ compre4qui-api/src/test/java/com/widneydev/compre4qui/Compre4quiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.widneydev.compre4qui;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Compre4quiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
