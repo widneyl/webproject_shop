e76ec108e1d93c55564b2306f5dfb3d113707f59
Widney Lima <widneylima21@gmail.com>
2024-07-03 23:38:33 -0300

    Creating route to show products

--- compre4qui-api/src/main/java/com/widneydev/compre4qui/control/Control.java
+++ compre4qui-api/src/main/java/com/widneydev/compre4qui/control/Control.java
@@ -1,16 +1,16 @@
 package com.widneydev.compre4qui.control;
 
 import org.springframework.web.bind.annotation.RestController;
-
 import com.widneydev.compre4qui.model.Product;
 import com.widneydev.compre4qui.repository.Repository;
+
+import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-
 
 @RestController
 public class Control {
@@ -36,5 +36,10 @@ public class Control {
     @PostMapping("/register")
     public Product register(@RequestBody Product p){
         return action.save(p);
+    }
+
+    @GetMapping("/select")
+    public List<Product> select(){
+        return action.findAll();
     }
 }
--- compre4qui-api/src/main/java/com/widneydev/compre4qui/repository/Repository.java
+++ compre4qui-api/src/main/java/com/widneydev/compre4qui/repository/Repository.java
@@ -1,9 +1,12 @@
 package com.widneydev.compre4qui.repository;
 
+import java.util.List;
+
 import org.springframework.data.repository.CrudRepository;
 import com.widneydev.compre4qui.model.Product;
 
 
 public interface Repository extends CrudRepository<Product, Integer>{
     
+    List<Product> findAll();
 }
