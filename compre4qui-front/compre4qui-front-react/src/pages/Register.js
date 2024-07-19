08a0ff66864eec5c561657de132aa860c31cefa4
Widney Lima <widneylima21@gmail.com>
2024-07-19 14:20:24 -0300

    correction of the post method in product registration

--- compre4qui-front/compre4qui-front-react/src/pages/Register.js
+++ compre4qui-front/compre4qui-front-react/src/pages/Register.js
@@ -1,40 +1,49 @@
-import React, { useState } from 'react'
-import axios from 'axios';
+    import React, { useState } from 'react'
+    import axios from 'axios';
 
-export default function Register() {
+    export default function Register() {
 
-    const [name, setname] = useState("");
-    // const [password, setPassword] = useState("");
+        const [productName, setname] = useState("");
+        // const [password, setPassword] = useState("");
 
-    function registerApi() {
-        fetch("http://localhost:8080/api/select")
-        .then(response => response.json())
-        .then(list =>{
-            console.log("ERRO")
-        })
-    }
+        async function verification(e) {
+            e.preventDefault();
+            
+            if (productName.length === 0) {
+                console.log("Enter for name")
+            }
+            else {
 
-    // function eventRegister(e) {
-    //     e.preventDefault()
-    //     if (name !== "") {
-    //         registerApi();
-    //     }
-    //     else {
-    //         console.log("Digite seu nome");
-    //     }
-    // }
-        return (
-            <div className='container'>
-                <div className='row mt-5'>
+                console.log("Entrou na function")
 
-                    <form onSubmit={registerApi}>
-                        <div className="mb-3">
-                            <label htmlFor="exampleInputEmail1" className="form-label">Name</label>
-                            <input type="text" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" value={name} onChange={(e) => setname(e.target.value)} />
-                        </div>
-                        <button type="submit" className="btn btn-primary">Submit</button>
-                    </form>
-                </div>
+                const dados = {
+                    name: productName
+                };
+
+                axios
+                    .post("http://localhost:8080/api/register", dados)
+                    .then((response) => {
+                        console.log(response.data)
+                    })
+                    .catch((err) => {
+                        console.log("Ocorreu um erro na requisição")
+                    })
+            }
+        }
+
+    return (
+
+        <div className='container'>
+            <div className='row mt-5'>
+
+                <form onSubmit={verification}>
+                    <div className="mb-3">
+                        <label htmlFor="exampleInputEmail1" className="form-label">Product Name</label>
+                        <input type="text" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" value={productName} onChange={(e) => setname(e.target.value)} />
+                    </div>
+                    <button type="submit" className="btn btn-primary" >Submit</button>
+                </form>
             </div>
-        )
-    }
+        </div>
+    )
+}
