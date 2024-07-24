9c3e5372dafc0a175ee8e4a63fea2fbc2a912ce8
Widney Lima <widneylima21@gmail.com>
2024-07-23 22:46:18 -0300

    Start of using useeffect in react to render objects registered on the user's screen on the registration page

--- compre4qui-front/compre4qui-front-react/src/pages/Register.js
+++ compre4qui-front/compre4qui-front-react/src/pages/Register.js
@@ -1,54 +1,96 @@
-import React, { useState } from 'react'
+import React, { useState} from 'react'
 import axios from 'axios';
 
 export default function Register() {
 
     const [productName, setname] = useState("");
-    // const [password, setPassword] = useState("");
+    const [productcategory, setProductCategory] = useState("");
+    const [productValue, setProductValue] = useState(false)
+    // const [showProducts, setShowProducts] = useState([])
 
-    // function alertSuccess(e) {
-    //     e.preventDefault();
-    //     <div class="alert alert-success alert-dismissible">
-    //         <button className="btn-close" data-bs-dismiss="alert">x</button>
-    //     </div>
-    // }
 
+
+    //Function to verification and send for api
     async function verification(e) {
         e.preventDefault();
 
         if (productName.length === 0) {
             console.log("Enter for name")
+            console.log(`O productValue esta como ${productValue}`)
         }
         else {
             console.log("Entrou na function")
+            console.log(`A categoria do produto é: ${productcategory}`)
+
 
             const dados = {
                 name: productName
             };
 
-            axios.post("http://localhost:8080/api/register", dados)
-            .then((response) => {
-                console.log(response.data)
-            }).catch((err) => {
-                console.log("Ocorreu um erro na requisição")
-            })
+            //Request POST
+            await axios.post("http://localhost:8080/api/register", dados)
+                .then((response) => {
+                    console.log(response.data)
+                }).catch((err) => {
+                    console.log("Ocorreu um erro na requisição")
+                })
+
+            setProductValue(true);
         }
     }
+
+    //Retorno
+
+    // function compriment() {
+    //     if (productValue) {
+    //         return <div>
+    //             <div>
+    //                 <h5>Você cadastrou um novo produto! Toque <button onClick={show} className='btn btn-success text-decoration-none'>aqui</button> para ver seus produtos</h5>
+    //             </div>
+    //         </div>
+    //     }
+    // }
+
+    // async function show() {
+
+    //     const req = await axios.get("http://localhost:8080/api/select")
+    //         .then((response) => response.data)
+    //         .catch((err) => console.log("Ocorreu um erro na requisição"));
+
+    //     setShowProducts(req);
+
+    //     return <div>
+    //         <ul>
+    //             {showProducts.map(obj => (<li>{obj.name}</li>))}
+    //         </ul>
+    //     </div>
+    // }
 
 
     return (
 
         <div className='container'>
             <div className='row mt-5'>
-
                 <form onSubmit={verification}>
                     <div className="mb-3">
-                        <label htmlFor="exampleInputEmail1" className="form-label">Product Name</label>
-                        <input type="text" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" value={productName} onChange={(e) => setname(e.target.value)} />
+                        {/* <label className="form-label">Product Name</label> */}
+                        <input type="text" placeholder='Product name' className="form-control" id="InputEmail1" aria-describedby="emailHelp" value={productName} onChange={(e) => setname(e.target.value)} />
+
+                        {/* <label className="form-label">What your the category product?</label> */}
+                        <select className="form-select mt-3" aria-label="Default select example" value={productcategory} onChange={(e) => setProductCategory(e.target.value)}>
+                            <option value="">What is your product category?</option>
+                            <option value="1">Technology</option>
+                            <option value="2">Clothes</option>
+                            <option value="3">Accessories</option>
+                        </select>
+
+
+                        <button type="submit" className="btn btn-success mt-3" >Submit</button>
                     </div>
-                    <button type="submit" className="btn btn-primary" >Submit</button>
                 </form>
             </div>
+            {compriment()}
         </div>
+
     )
 }
